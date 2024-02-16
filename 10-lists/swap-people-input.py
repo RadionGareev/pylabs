f1cc55ea3ee2846fae75ce277df0f4df4eb46db2
Radion <radeon_service@mail.ru>
2024-02-16 22:48:10 +0200

    HW2-swapPeople

--- 10-lists/swap-people-input.py
+++ 10-lists/swap-people-input.py
@@ -4,10 +4,16 @@ people = ["Jony","Mary","Francis","Fernando"]
 print("People in list:\n")
 for i in range(len(people)):
     print(i+1, people[i])
-want = input("would you like to renum some of them(y/n?\n>>> ")=="y"
+want = input("would you like to renum some of them(y/n)?\n>>> ")=="y"
 if want:
     select_who = int(input("wich number? \n>>> "))-1
-    select_where = int(input("where you would like to replece it(number)? \n>>> "))-1                
+    if select_who > len(people) or select_who < 0:
+        quit("Write correct number! " )
+    select_where = int(input("where you would like to replece it(number)? \n>>> "))-1
+    if select_where > len(people) or select_where < 0 or select_where == select_who:
+        quit("Write correct number! " )    
+else:
+    quit("Okay, bye!")                
 temp        =people[select_who]
 people[select_who]   =people[select_where]
 people[select_where]   =temp
