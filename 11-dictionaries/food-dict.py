753071d6e3e6b0f2da8338df977f76e6482d6d09
Radion <radeon_service@mail.ru>
2024-02-26 17:50:30 +0200

    HW-FoodDelivery

---
+++ 11-dictionaries/food-dict.py
@@ -0,0 +1,33 @@
+from os import system
+order = {
+    'client'    :   'John Doe',
+    'item'      :   'Salad',
+    'quantity'  :   3,
+    'price'     :   15.00
+}
+order['total'] = order['price'] * order['quantity']
+#HW1 - 20% discount in case  if more than 7 items
+if order['quantity'] >7:
+    order['total']*=0.8
+
+system('cls')
+print("ORDER for    :", order['client'])
+print("Food         :", order['item'])
+print("Price x qty  :", order['price'], ' x ', order['quantity'],'\n')
+print("Total        :", order['total'])
+if order['total'] >300:
+    print('You odered more than 300$. You can benefit free delivery.  ')
+    order['delivery'] = 'free'
+else:
+    print('You odered less than 300$. Deelivery will cost 50$.  ')
+    order['delivery'] = 50
+
+#HW2
+delivery_want = input("\n Do you want delivery? y/n \n>>>")=='y'
+if  delivery_want and order['total']>300:
+    print(f"you got {order['delivery']} delivery. \n\n\n Total  sum  is: {order['total']} \n Enjoy!")
+if delivery_want and order['total']<300:
+    order['total'] = order['total']+order['delivery']
+    print(f"You should pay {order['delivery']} $ for delivery. \n\n Total  sum  is: {order['total']} ")
+#else:
+#    print(' Got it,  no delivery. Come and take your command')
