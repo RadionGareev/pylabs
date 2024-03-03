c12f23a31699e74b33657b54952f412d288cbe9e
Radion <radeon_service@mail.ru>
2024-03-03 13:48:28 +0200

    HW_math functions

---
+++ 12-functions/math-functions.py
@@ -0,0 +1,16 @@
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+#HW1 2 funct add and sub
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def hw_func(a,b,c,d):
+    return a+b*c/d
+r=add(10,100)
+#hw2 rewrite r = 1+2*3/4
+r2=hw_func(1,2,3,4)
+print(r)
+print(r2)
